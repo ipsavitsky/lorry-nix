8eb12df918f762470134462ffe720f7003e219cb
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-03-13 09:41:58 +0000

    Initialize lorry package

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = pkgs.rust-bin.stable."1.84.0".minimal;
+          rustc = pkgs.rust-bin.stable."1.84.0".minimal;
+        };
+
+        lorrySrc = pkgs.fetchFromGitLab {
+          group = "CodethinkLabs";
+          owner = "lorry";
+          repo = "lorry2";
+          rev = "v2.1.0";
+          sha256 = "sha256-tVrkKNrmcST2kpryJzJTtUKmdanb0HjpHMSmdaSsb3I=";
+        };
+      in
+      {
+        packages = rec {
+          default = lorry;
+          lorry = rustPlatform.buildRustPackage {
+            name = "lorry2";
+            src = lorrySrc;
+
+            cargoLock = {
+              lockFile = "${lorrySrc}/Cargo.lock";
+            };
+
+            buildAndTestSubdir = "lorry";
+
+            buildInputs = with pkgs; [
+              git
+              openssl
+            ];
+
+            nativeBuildInputs = with pkgs; [
+              git
+              openssl
+              sqlx-cli
+              pkg-config
+            ];
+
+            OPENSSL_NO_VENDOR = 1;
+            SQLX_OFFLINE = true;
+
+            meta.platforms = pkgs.lib.platforms.linux;
+          };
+        };
+      }
+    );
+}
